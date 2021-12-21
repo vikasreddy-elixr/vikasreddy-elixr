774210bc63f36776ef01a2c87ce34de756e14eb2
vikasreddy-elixr <95477225+vikasreddy-elixr@users.noreply.github.com>
2021-12-21 14:51:24 +0530

    In this Code the file formatting has been fixed.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,7 +11,7 @@ import java.lang.String;
 public class Main {
     public static void main(String[] args) {
 
-        if(args.length!=2) {
+        if (args.length != 2) {
             System.out.println("Please enter the valid path and word");
             return;
         }
@@ -20,52 +21,54 @@ public class Main {
 
         isFileFormatSupported(file);
 
-        if(!file.exists() || !isFileFormatSupported(file)) {
+        if (!file.exists() || !isFileFormatSupported(file)) {
             System.out.println("file is not valid");
-            return ;
+            return;
         }
-        if(file.length() == 0) {
+        if (file.length() == 0) {
             System.out.println("the file does not contains any data ");
-            return ;
+            return;
         }
         System.out.println("processing................");
         String data = readFileAsString(txtFilePath);
         searchTheWord(data, searchword);
     }
-        public static String readFileAsString(String fileName) {
-                String data = "";
-              try {
-                   data = new String(Files.readAllBytes(Paths.get(fileName)));
-              } catch (IOException e) {
-                  e.printStackTrace();
-              }
-              return data.replaceAll("[^a-zA-Z0-9@]", " ");
-    }
-        public static boolean isFileFormatSupported(File file) {
-           return (file.getName().endsWith(".txt"));
+
+    public static String readFileAsString(String fileName) {
+        String data = "";
+        try {
+            data = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        public static void searchTheWord(String data,String searchword) {
-                StringTokenizer st = new StringTokenizer(data);
-                int count = 0;
+        return data.replaceAll("[^a-zA-Z0-9@]", " ");
+    }
 
-                while (st.hasMoreTokens())
-                {
+    public static boolean isFileFormatSupported(File file) {
+        return (file.getName().endsWith(".txt"));
+    }
 
-                        if (searchword.equalsIgnoreCase(st.nextToken())) {
-                            count++;
-                        }
+    public static void searchTheWord(String data, String searchword) {
+        StringTokenizer st = new StringTokenizer(data);
+        int count = 0;
 
-                }
+        while (st.hasMoreTokens()) {
 
-                if (count == 0) {
-                    System.out.println("Word not found");
-                } else {
-                    System.out.println("The word has been found");
-                    System.out.println("the word has been repeated for " + count + " times");
-                }
+            if (searchword.equalsIgnoreCase(st.nextToken())) {
+                count++;
             }
 
         }
+
+        if (count == 0) {
+            System.out.println("Word not found");
+        } else {
+            System.out.println("The word has been found");
+            System.out.println("the word has been repeated for " + count + " times");
+        }
+    }
+
+}
 
 
 
