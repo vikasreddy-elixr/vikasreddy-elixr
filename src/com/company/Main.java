39b85cf97bbeefc885f541c5d4b0de25344d743a
vikasreddy-elixr <95477225+vikasreddy-elixr@users.noreply.github.com>
2021-12-21 15:47:35 +0530

    Made changes in the print statements.created variables for ".txt" and "[^a-zA-Z0-9@]" instead of using those directly.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,43 +8,44 @@ import java.lang.String;
 
 
 public class Main {
+    final static String extention=".txt";
+    final static String specialChars="[^a-zA-Z0-9@]";
+    final static String replacement=" ";
     public static void main(String[] args) {
 
         if (args.length != 2) {
-            System.out.println("Please enter the valid path and word");
+            System.out.println("Exiting, since no file path and word to search for are provided!");
             return;
         }
         String txtFilePath = args[0];
         String searchword = args[1];
         File file = new File(txtFilePath);
 
-        isFileFormatSupported(file);
-
         if (!file.exists() || !isFileFormatSupported(file)) {
-            System.out.println("file is not valid");
+            System.out.println("File is not valid");
             return;
         }
         if (file.length() == 0) {
-            System.out.println("the file does not contains any data ");
+            System.out.println("The file does not contains any data ");
             return;
         }
-        System.out.println("processing................");
+        System.out.println("Processing................");
         String data = readFileAsString(txtFilePath);
         searchTheWord(data, searchword);
     }
 
     public static String readFileAsString(String fileName) {
-        String data = "";
+        String data =null;
         try {
             data = new String(Files.readAllBytes(Paths.get(fileName)));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return data.replaceAll("[^a-zA-Z0-9@]", " ");
+        return data.replaceAll(specialChars, replacement);
     }
 
     public static boolean isFileFormatSupported(File file) {
-        return (file.getName().endsWith(".txt"));
+        return (file.getName().endsWith(extention));
     }
 
     public static void searchTheWord(String data, String searchword) {
@@ -61,7 +61,7 @@ public class Main {
             System.out.println("Word not found");
         } else {
             System.out.println("The word has been found");
-            System.out.println("the word has been repeated for " + count + " times");
+            System.out.println("The word has been repeated for " + count + " times");
         }
     }
 }
